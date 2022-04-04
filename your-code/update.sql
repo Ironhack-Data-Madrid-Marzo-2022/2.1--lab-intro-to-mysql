7c8168e8968b0378e628253fc0843f6fd2094bc5
kaaiik <enrperlop@gmail.com>
2022-04-05 00:33:00 +0200

    listo

---
+++ your-code/update.sql
@@ -0,0 +1,11 @@
+update customers 
+set Email_ = 'ppicaso@gmail.com' 
+where `Name_` = 'Pablo Picasso';
+
+update customers 
+set Email_ = 'lamarr_hd@hollywood.com' 
+where `Name_` = 'Hedy Lamarr';
+
+update customers 
+set Email_ = 'hello@nasa.gov' 
+where `Name_` = 'Katherine Johnson';
